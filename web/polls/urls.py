e54b5a18abb116f9e68e4ad4c7ea25893895ed14
Da Yeong <yoon351200@naver.com>
2023-05-15 15:54:25 +0900

    음성 변환 & 로딩 구현

---
+++ web/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('analysis/', views.analysis, name = 'analysis'),
+    path('login/', views.login, name = 'login'),
+    path('mypage/', views.mypage, name = 'mypage'),
+    path('recording/', views.recording, name = 'recording'),
+    path('signup.', views.signUp, name = 'signUp'),
+]
---
+++ web/polls/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def index(request):
+    template = loader.get_template("polls/index.html")
+    return render(request, "polls/index.html")
+
+def analysis(request):
+    template = loader.get_template("polls/analysis.html")
+    return render(request, "polls/analysis.html")
+
+def login(request):
+    template = loader.get_template("polls/login.html")
+    return render(request, "polls/login.html")
+
+def mypage(request):
+    template = loader.get_template("polls/mypage.html")
+    return render(request, "polls/mypage.html")
+
+def signUp(request):
+    template = loader.get_template("polls/signUp.html")
+    return render(request, "polls/signUP.html")
+
+def recording(request):
+    template = loader.get_template("polls/recording.html")
+    return render(request, "polls/recording.html")
